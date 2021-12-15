29fb3bee631be9c3a8cd07f5d8c60bf546c387cb
predator1245 <jashshah37@yahoo.com>
2021-12-15 22:50:34 +0100

    Implement Spooky event

--- Assets/Scripts/SpookyTimeEvent.cs
+++ Assets/Scripts/SpookyTimeEvent.cs
@@ -3,11 +3,27 @@
 [CreateAssetMenu(fileName = "SpookyTime", menuName = "ICalendarEvent/SpookyTime")]
 public class SpookyTimeEvent : ICalendarEvent
 {
+    private bool m_is_active = false;
+    private GameObject m_weather_manager;
+
     public override void Init()
     {
+        m_weather_manager = GameObject.Find("WeatherManager");
     }
 
     public override void OnEvent(int currentDay)
     {
+        if (currentDay < startDate || currentDay > endDate)
+        {
+            if (m_is_active)
+            {
+                m_is_active = false;
+                m_weather_manager.GetComponent<WeatherManager>().SetDefaultSunColor();
+            }
+            return;
+        }
+
+        m_weather_manager.GetComponent<WeatherManager>().SetDarkIntensity();
+        m_is_active = true;
     }
 }
--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -10,13 +10,16 @@ public class WeatherManager : MonoBehaviour
     public GameObject Sun;
     
     private Color m_default_sun_color;
+    private float m_default_sun_intensity;
     private static readonly Color WINTER_SUN_COLOR = new Color(0.6941177f, 0.8192417f, 0.8826951f, 1f);
     private static readonly Color BRIGHT_SUN_COLOR = new Color(0.8113208f, 0.8679245f, 0.5764329f, 1f);
+    private static readonly float DARK_INTENSITY = 0.3f;
 
     // Start is called before the first frame update
     void Start()
     {
         m_default_sun_color = Sun.GetComponent<Light2D>().color;
+        m_default_sun_intensity = Sun.GetComponent<Light2D>().intensity;
         Debug.Log(m_default_sun_color);
     }
 
@@ -49,5 +52,11 @@ public class WeatherManager : MonoBehaviour
     public void SetDefaultSunColor()
     {
         Sun.GetComponent<Light2D>().color = m_default_sun_color;
+        Sun.GetComponent<Light2D>().intensity = m_default_sun_intensity;
+    }
+
+    public void SetDarkIntensity()
+    {
+        Sun.GetComponent<Light2D>().intensity = DARK_INTENSITY;
     }
 }
